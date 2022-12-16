cd82b69bbf2818d057c736dc8c488090626fa802
grtcdr <ba.tahaaziz@gmail.com>
2022-12-16 02:45:38 +0100

    Clean up

--- lisp/publish.el
+++ lisp/publish.el
@@ -1,48 +1,49 @@
-;; Load packages
 (require 'ox-publish)
-
-;;; Configurations:
-
-;; Org caching:
-
-(setq org-export-time-stamp-file nil
-      org-publish-timestamp-directory ".cache/")
-
-;; Org content metadata:
-
-(setq org-html-metadata-timestamp-format "%B %d, %Y")
-
-;; Org source blocks:
-
-(setq org-src-fontify-natively t
-      org-src-preserve-indentation t
-      org-html-htmlize-output-type nil)
-
-;; Emacs:
-
-(setq make-backup-files nil)
-
-;; Metadata:
+(require 'shr)
 
 (setq user-full-name "Aziz Ben Ali"
-      user-mail-address "ba.tahaaziz@gmail.com")
+      user-mail-address "tahaaziz.benali@esprit.tn"
+      make-backup-files nil)
 
-(defun read-template (path)
-  "Read a template from the  directory."
-  (with-temp-buffer
-    (insert-file-contents
-     (file-name-concat "src/templates" path))
-    (buffer-string)))
+(setq org-publish-timestamp-directory ".cache/"
+      org-export-time-stamp-file nil
+      org-src-fontify-natively t
+      org-src-preserve-indentation t
+      org-html-postamble nil
+      org-html-head-include-default-style nil
+      org-html-htmlize-output-type nil
+      org-html-metadata-timestamp-format "%B %d, %Y")
 
-(defun publish-html-head ()
-  "<link rel=\"stylesheet\" href=\"https://grtcdr.tn/css/main.css\" />")
+(defun site/link (href)
+  "Format the stylesheet located at HREF as a ’link’ tag."
+  (shr-dom-to-xml
+   `(link ((rel . "stylesheet")
+	   (href . ,href)))))
 
-;;; Project specification:
+(defvar site/html-head
+  (concat
+   (site/link "https://grtcdr.tn/css/common.css")
+   (site/link "https://grtcdr.tn/css/heading.css")
+   (site/link "https://grtcdr.tn/css/source.css")
+   (site/link "https://grtcdr.tn/css/table.css")
+   (site/link "https://grtcdr.tn/css/nav.css")
+   (site/link "https://grtcdr.tn/css/org.css"))
+  "HTML headers shared across projects.")
+
+(defvar site/preamble
+  (shr-dom-to-xml
+   '(nav nil
+	 (ul nil
+	     (li nil
+		 (a ((href . "/"))
+		    "home"))
+	     (li nil
+		 (a ((href . "https://github.com/vivmaniero"))
+		    "github")))))
+  "Define an HTML snippet/template used as a preamble across all projects.")
 
 (setq org-publish-project-alist
-      (let ((content-preamble (read-template "preamble/content.html"))
-	    (html-head (publish-html-head)))
-	(list
+      (list
 	 (list "content"
 	       :base-extension "org"
 	       :base-directory "src"
@@ -50,10 +51,8 @@
 	       :publishing-function 'org-html-publish-to-html
 	       :html-doctype "html5"
 	       :html-html5-fancy t
-	       :html-preamble content-preamble
-	       :html-postamble nil
-	       :html-head-extra html-head
-	       :html-head-include-default-style nil)
+	       :html-preamble site/preamble
+	       :html-head-extra site/html-head)
 	 (list "docs"
 	       :base-extension "org"
 	       :base-directory "src/docs"
@@ -64,10 +63,8 @@
 	       :with-toc t
 	       :html-doctype "html5"
 	       :html-html5-fancy t
-	       :html-preamble content-preamble
-	       :html-postamble nil
-	       :html-head-extra html-head
-	       :html-head-include-default-style nil)
+	       :html-preamble site/preamble
+	       :html-head-extra site/html-head)
 	 (list "assets"
 	       :base-extension "webp\\|png\\|jpe?g\\|svg"
 	       :base-directory "assets"
@@ -75,4 +72,4 @@
 	       :publishing-function 'org-publish-attachment
 	       :recursive t)
 	 (list "all"
-	       :components (list "content" "docs" "assets")))))
+	       :components (list "content" "docs" "assets"))))
